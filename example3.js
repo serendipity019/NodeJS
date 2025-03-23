ae41c6eb2fb98b5ccbbf03267ae7adcdd2c68813
serendipity019 <navagio09@gmail.com>
2025-03-23 19:48:31 +0200

    feat: we created an example with server, also we write and read file

---
+++ example3.js
@@ -0,0 +1,35 @@
+const fs =require('fs');
+const http =require('http');
+const os = require('os');
+
+const osType = os.type();
+//console.log(osType);
+
+const htmlContent = `
+ <!DOCTYPE html> 
+ <html>
+ <h3> Hello, World! your OS type is ${osType}. </h3>
+ </html>
+`
+
+const server = http.createServer((req, res) => {
+    console.log("Initial we create index.html file with content: htmlContent");
+    fs.writeFile('./index.html', htmlContent, err => {
+        if (err) {
+            console.log("Problem in writing file");
+        } else {
+            console.log("We read the file and return it back");
+            fs.readFile('index.html', 'utf8', (err, content) => {
+                if (err) {
+                    console.log('Problem in reading file', err);
+                }
+                res.setHeader("content-Type", "text-html");
+                res.end(content);
+            })
+        }
+    })
+});
+
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+})
