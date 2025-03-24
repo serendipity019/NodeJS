0c6eb1440e848457abf138a1a22310098bb417b9
serendipity019 <navagio09@gmail.com>
2025-03-24 18:41:18 +0200

    feat: add post method

--- myapp2/app.js
+++ myapp2/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("This is the home page");
+})
 
+app.post('/user', (req, res) => {
+    let data = req.body;
 
+    console.log(data);
+    res.send("User page");
+})
 
 
 app.listen(port, () => {
