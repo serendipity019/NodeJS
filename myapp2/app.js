8e8fa8191f33229b2051bb2b582bef2a20a218c2
serendipity019 <navagio09@gmail.com>
2025-03-28 22:07:04 +0200

    feat: we make an example to post data from thunder and from a form

--- myapp2/app.js
+++ myapp2/app.js
@@ -2,18 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})); //This pass the data from a html form 
+app.use('/', express.static('files'));
+
 app.get('/', (req, res) => {
     res.send("This is the home page");
-})
+});
 
 app.post('/user', (req, res) => {
     let data = req.body;
-
+    let username = req.body.username;
+    let email = req.body.email;
     console.log(data);
-    res.send("User page");
-})
+    res.send(`User data ${username}, ${email}`);
+});
 
+app.post('/userForm', (req, res) => {
+    let data = req.body;
+
+    console.log("Data", data); 
+    res.send("UserForm Page");
+});
+
+app.use('/user1', (req, res) => {
+    console.log('User 1');
+    res.send('User 1 Page');
+});
+
+app.use('/user2', (req, res) => {
+    console.log('User 2');
+    res.send('User 2 Page');
+});
 
 app.listen(port, () => {
     console.log("Server is up");
-})
+});
