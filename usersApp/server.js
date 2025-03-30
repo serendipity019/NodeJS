d6ed07502ce4c550a6ea153235154985f27c6803
serendipity019 <navagio09@gmail.com>
2025-03-30 16:45:25 +0300

    feat: We start to create userApp

---
+++ usersApp/server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const app = require('./app');
+const port = 3000; 
+
+mongoose.connect('')
+    .then(
+        () => {
+            console.log("Connection to MongoDB established");
+        },
+        err => {console.log('Failed to connect to MongoDB', err); }
+    ); 
+
+app.listen(port, (req, rs) => {
+    console.log('Server is up');
+});
