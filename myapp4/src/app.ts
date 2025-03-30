ebe5bad79d4311e74b4ea6dce33470048befbc3c
serendipity019 <navagio09@gmail.com>
2025-03-30 16:05:56 +0300

    feat: app.ts and util.ts created

---
+++ myapp4/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express'; 
+import {greet} from './utils'; 
+
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) =>{
+    res.send("Hello typescript");
+});
+
+app.get('/greetings', (req,res) => {
+    const message = greet("World War 3");
+    res.send(message);
+});
+
+app.listen(port, () => {
+    console.log('Server is up');
+});
+
