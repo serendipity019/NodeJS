df8845d58dab0b0a3a86e45e8b344e3c94558ee3
kleidimos <navagio09@gmail.com>
2025-04-02 01:42:55 +0300

    feat: we created services folder and also in user.controller add status code

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -1,12 +1,14 @@
 const User = require('../models/user.model');
+const userService = require('../services/user.services');
 
 exports.findAll = async(req, res) => {
     console.log('Find all users from collection users');
 
     try {
-        const result = await User.find();
+        //const result = await User.find();
+        const result = await userService.findAll();
 
-        res.json({status: true, data: result});
+        res.status(200).json({status: true, data: result});
     } catch (err) {
         console.log('Problem in reading users', err);
         res.json({status: false, data: err});
@@ -18,11 +20,16 @@ exports.findOne = async(req, res) => {
     let username = req.params.username;
 
     try {
-        const result = await User.findOne({username: username});
-        res.json({status:true, data: result});
+        //const result = await User.findOne({username: username});
+        const result = await userService.findOne(username);
+        if (result) {
+            res.status(200).json({status:true, data: result});
+        } else {
+            res.status(404).json({status: false, data: 'User not exists'});
+        }
     } catch (err) {
         console.log('Problem in finding user', err);
-        res.json({status: false, data: err});
+        res.staus(400).json({status: false, data: err});
     }
 }
 
@@ -45,9 +52,32 @@ exports.create = async(req, res) => {
 
     try {
         const result = await newUser.save();
-        res.json({status: true, data: result});
+        res.status(200).json({status: true, data: result});
     } catch(err) {
         console.log('Problem in creating user', err);
-        res.json({status: false, data: err}); 
+        res.status(400).json({status: false, data: err}); 
+    }
+}
+
+exports.update = async(req, res) => {
+    const username = req.body.username; 
+
+    console.log('Update user with username', username);
+    const updateUser = {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        address: {
+            area: req.body.address.area,
+            road: req.body.address.road
+        }
+    };
+    
+    try {
+        const result = await User.findOneAndUpdate({username:username}, updateUser, {new: true}); // We write new: true because in diferent case this will return to us the old data.
+        res.status(200).json({status:true, data: result});
+    } catch (err) {
+        console.log('Problem in updating user', err);
+        res.status(400).json({status: false, data: err});
     }
 }
