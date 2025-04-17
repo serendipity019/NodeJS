34dee4218af1bbdc0d3e1e494f67ac7a97ce616b
serendipity019 <navagio09@gmail.com>
2025-04-17 19:56:28 +0300

    feat: we make other 2 tests for post api to see if create user

--- usersApp/__tests__/user.test.js
+++ usersApp/__tests__/user.test.js
@@ -55,12 +55,35 @@ describe("Requests for /api/users", () => {
                 'password' : '12345',
                 'name': 'test5 name',
                 'surname': 'test5 surname',
-                'email' : 'test5@aueb.gr'
+                'email' : 'test5@aueb.gr',
+                'address': {
+                    'area': 'area1',
+                    'road' : 'road5'
+                }
             });
         
             expect(res.statusCode).toBe(200);
             expect(res.body.status).toBeTruthy();
-    }, 20000)
+    }, 20000);
+
+    it('POST Creates a user that already exists', async () => {
+        const res = await request(app)
+        .post('/api/users')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            'username' : 'test5',
+                'password' : '12345',
+                'name': 'new name',
+                'surname': 'new surname',
+                'email' : 'new@aueb.gr',
+                'address': {
+                    'area': 'area1',
+                    'road' : 'road5'
+        }}); 
+        
+        expect(res.statusCode).toBe(400);
+        expect(res.body.status).not.toBeTruthy();
+    }, 10000);
 });
 
 describe("Requests for /api/users/:username", () => {
