ebef03a95e3d866c5c9287f7868dff99ef558977
serendipity019 <navagio09@gmail.com>
2025-05-14 00:46:14 +0300

    feat: i change in app.js the ap.use to origin: '*'

--- usersApp/app.js
+++ usersApp/app.js
@@ -12,9 +12,10 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes');
 const auth = require('./routes/auth.routes');
 
-// app.use(cors({
-//     origin: ['http://localhost:8000'] //This is allow to have access the python server 
-// }));
+app.use(cors({
+    origin: '*'
+    //origin: ['http://localhost:8000'] //This is allow to have access the python server 
+}));
 
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
